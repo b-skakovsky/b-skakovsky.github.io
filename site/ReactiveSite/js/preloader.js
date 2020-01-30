9a782d6d8808ff43466c76745bf8c2f48a151be5
Bogdan Skakovsky <bogdanskakovsky95@gmail.com>
2020-01-30 10:03:01 +0200

    fix city

--- site/ReactiveSite/js/preloader.js
+++ site/ReactiveSite/js/preloader.js
@@ -1,9 +1,7 @@
 document.body.onload = function(){
-    setTimeout(function(){
-        var preloader = document.getElementById('page-preloader');
-        if( !preloader.classList.contains('done'))
-        {
-            preloader.classList.add('done');
-        }
-    })
+    var preloader = document.getElementById('page-preloader');
+    if( !preloader.classList.contains('done'))
+    {
+        preloader.classList.add('done');
+    }
 }
